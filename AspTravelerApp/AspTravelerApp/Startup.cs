3eb8cfbfc81f04375c81cc045c62c8dfe8b3f3b8
p00293412 <jakob.hendrix@la.gov>
2019-03-15 16:43:23 -0500

    Added basic appsettings file

--- AspTravelerApp/AspTravelerApp/Startup.cs
+++ AspTravelerApp/AspTravelerApp/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using System;
@@ -9,6 +10,17 @@ namespace AspTravelerApp
 {
     public class Startup
     {
+        // Set runtime environment configuration
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                            .SetBasePath(env.ContentRootPath)
+                            .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+        }
+        public IConfiguration Configuration { get; set; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
