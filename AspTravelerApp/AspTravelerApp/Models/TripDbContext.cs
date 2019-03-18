07e96d0c9dedb0e80a3b90e7a2618e98a52b55e7
p00293412 <jakob.hendrix@la.gov>
2019-03-18 16:42:25 -0500

    Added our initial foray into the entity framework, including ignoring any sqlite dbs.

---
+++ AspTravelerApp/AspTravelerApp/Models/TripDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AspTravelerApp.Models
+{
+    public class TripDbContext : DbContext
+    {
+        public TripDbContext(DbContextOptions < TripDbContext > options) : base(options)
+        {
+            Database.EnsureCreatedAsync().Wait();
+        }
+
+        public DbSet<Trip> Trips { get; set; }
+    }
+}
--- AspTravelerApp/AspTravelerApp/Startup.cs
+++ AspTravelerApp/AspTravelerApp/Startup.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using AspTravelerApp.Models;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Sqlite;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -33,6 +36,10 @@ namespace AspTravelerApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TripDbContext>(options => 
+                options.UseSqlite(Configuration.GetConnectionString("DefaultConnection"))
+            );
+
             services.AddMvc();
         }
 
