0234f511ef3c94fc541f69994beb7052741cb891
p00293412 <jakob.hendrix@la.gov>
2019-03-22 15:24:06 -0500

    Added the repo URL to project.cs

--- AspTravelerApp/AspTravelerApp/Program.cs
+++ AspTravelerApp/AspTravelerApp/Program.cs
@@ -5,6 +5,9 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace AspTravelerApp
 {
+    /// <summary>
+    /// Example of an ASP.NET MVC app. See the follow along code at https://github.com/csharpfritz/AspTravlerz
+    /// </summary>
     public class Program
     {
         public static void Main(string[] args)
